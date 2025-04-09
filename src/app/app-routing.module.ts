bbea8ce09f138692052f95c9d88ed082a0577c94
ybarrial <yefferson.barrial@avivel.com.pe>
2025-04-09 13:09:14 -0500

    add routes to first loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
