6141c62a92ab25be8c391fc188040b0fedfc5c67
ybarrial <yefferson.barrial@avivel.com.pe>
2025-04-10 17:18:55 -0500

    add company logo, add material component

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [],
   imports: [
+    MatCardModule,
     MatSidenavModule
   ],
   exports: [
+    MatCardModule,
     MatSidenavModule
   ]
 })
